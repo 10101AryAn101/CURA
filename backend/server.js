4a906b897831895b74263ec76b5aa3b4f334fc58
10101AryAn101 <aryanshukla9358@gmail.com>
2025-12-15 01:40:57 +0530

    changing...

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,24 @@ import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
 
-// app config
+
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// middlewares
-app.use(express.json())
-app.use(cors())
 
-// api endpoints
+app.use(express.json())
+app.use(cors({
+    origin: [
+        'https://cura-public.onrender.com', 
+        'https://cura-admin.onrender.com',     
+        'http://localhost:5173',
+        'http://localhost:5174'
+    ],
+    credentials: true
+}))
+
 app.use("/api/user", userRouter)
 app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
